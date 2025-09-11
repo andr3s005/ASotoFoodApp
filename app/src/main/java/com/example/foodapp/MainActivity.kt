ccf0d93d46f6180a701a99c01e3560a657d678b2
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-09-10 23:36:23 -0600

    Creacion de la barra superior

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -1,18 +1,39 @@
+@file:kotlin.OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.foodapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.OptIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.foodapp.ui.theme.FoodAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,17 +43,57 @@ class MainActivity : ComponentActivity() {
         setContent {
             FoodAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PantallaPrincipal()
+                    PantallaPrincipal(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@kotlin.OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PantallaPrincipal(){
-
+fun PantallaPrincipal(modifier: Modifier = Modifier){
+    Scaffold(
+    topBar = {
+        TopAppBar(
+            title = {},
+            navigationIcon = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .padding(start = 16.dp, end = 8.dp)
+                            .size(40.dp)
+                            .clip(CircleShape)
+                            .background(Color.Red),
+                        contentAlignment = Alignment.Center
+                    ){
+                        Icon(
+                            imageVector = Icons.Default.Person,
+                            contentDescription = "Perfil"
+                        )
+                    }
+                    Text(text = "Hola Andres")
+                }
+            },
+            actions = {
+                IconButton( onClick = {}) {
+                    Icon(
+                        imageVector = Icons.Default.ExitToApp,
+                        contentDescription = "Compartir",
+                        tint = Color.Red
+                    )
+                }
+            }
+        )
+    }
+    ){ innerPadding ->
+        Text(text = "Bienvenido",
+            modifier= Modifier.padding(innerPadding)
+        )
+    }
 }
 
 @Preview(showBackground = true)
