8bef20e22b68dbba88718c4c38f5048a78293d3c
andr3s005 <asg78886@lasallebajio.edu.mx>
2025-09-13 14:27:07 -0600

    Agregamos las categorias, restaurantes y comidas al Main

--- app/src/main/java/com/example/foodapp/MainActivity.kt
+++ app/src/main/java/com/example/foodapp/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.OptIn
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -32,8 +35,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.foodapp.ui.theme.FoodAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -67,32 +72,74 @@ fun PantallaPrincipal(modifier: Modifier = Modifier){
                             .padding(start = 16.dp, end = 8.dp)
                             .size(40.dp)
                             .clip(CircleShape)
-                            .background(Color.Red),
+                            .background(Color(0xFFFFA500)),
                         contentAlignment = Alignment.Center
                     ){
                         Icon(
                             imageVector = Icons.Default.Person,
-                            contentDescription = "Perfil"
+                            contentDescription = "Perfil",
+                            tint = Color.White
                         )
                     }
-                    Text(text = "Hola Andres")
+                    Text(
+                        text = "Hola Andres",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
+
                 }
             },
             actions = {
                 IconButton( onClick = {}) {
                     Icon(
                         imageVector = Icons.Default.ExitToApp,
-                        contentDescription = "Compartir",
-                        tint = Color.Red
+                        contentDescription = "Cerrar Sesion",
+                        tint = Color.Black
                     )
                 }
             }
         )
     }
     ){ innerPadding ->
-        Text(text = "Bienvenido",
-            modifier= Modifier.padding(innerPadding)
-        )
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(top = 16.dp)
+        ) {
+            Text(
+                text = "Nuestras Categorias",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            CategoryList()
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Busca los mejores restaurantes",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            RestaurantList()
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Nuestras mejores comidas",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FoodList()
+
+
+        }
     }
 }
 
